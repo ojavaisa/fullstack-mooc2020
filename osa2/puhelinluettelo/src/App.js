7d4e671dddb69ce0b891a522f92f295675f8bf54
Olli Väisänen <olli.vaisanen@iki.fi>
2020-11-27 18:39:14 +0200

    Tehtävä 2.8.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,9 +3,10 @@ import Persons from './components/Persons'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1231244' }
     ]);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
     const addPerson = (event) => {
         event.preventDefault();
@@ -14,28 +15,32 @@ const App = () => {
             setNewName('');
         } else {
             const personObject = {
-                name: newName
+                name: newName,
+                number: newNumber
             }
             setPersons(persons.concat(personObject));
             setNewName('');
+            setNewNumber('');
         }
     }
 
-    const handleFormChange = (event) => {
+    const handleNameChange = (event) => {
         console.log(event.target.value);
         setNewName(event.target.value);
+    }
+
+    const handleNumberChange = (event) => {
+        console.log(event.target.value);
+        setNewNumber(event.target.value);
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleFormChange}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
+                <div>name: <input value={newName} onChange={handleNameChange}/></div>
+                <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+                <div><button type="submit">add</button></div>
             </form>
             <h2>Numbers</h2>
             <div>debug: {newName}</div>
