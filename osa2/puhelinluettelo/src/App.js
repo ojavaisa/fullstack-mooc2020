45be686013d36e518e770e1333e6990d776d3af9
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-02 17:13:16 +0200

    Tehtävä 2.9.
    En keksinyt miten näytettävät henkilöt rajaava lista (<ul> {peopleToShow.map( ... )} </ul>) eriytetään komponentiksi People, joten nyt vaan Person.js, joka ei juuri tee mitään...

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import People from './components/People'
+import Person from './components/Person';
 
 const App = () => {
     const [people, setPeople] = useState([
@@ -7,58 +7,65 @@ const App = () => {
         { name: 'Ada Lovelace', number: '39-44-5323523' },
         { name: 'Dan Abramov', number: '12-43-234345' },
         { name: 'Mary Poppendieck', number: '39-23-6423122' }
-      ]);
+    ]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
-    const [peopleToShow, setPeopleToShow] = useState('');
+    const [filterString, setFilterString] = useState('');
 
     const addPerson = (event) => {
         event.preventDefault();
-        if(people.map(person => person.name).includes(newName)) {
-            alert(`${newName} is already added to phonebook.`)
+        if (people.map(person => person.name).includes(newName)) {
+            alert(`${newName} is already added to phonebook.`);
             setNewName('');
+            setNewNumber('');
         } else {
             const personObject = {
                 name: newName,
                 number: newNumber
-            }
+            };
             setPeople(people.concat(personObject));
             setNewName('');
             setNewNumber('');
         }
-    }
+    };
 
     const handleNameChange = (event) => {
         //console.log(event.target.value);
         setNewName(event.target.value);
-    }
+    };
 
     const handleNumberChange = (event) => {
         //console.log(event.target.value);
         setNewNumber(event.target.value);
-    }
+    };
 
     const filterPeople = (event) => {
-        const peopleToShow = people.filter(person => 
-            person.name.toLowerCase().startsWith(event.target.value.toLowerCase()));
-        console.log(peopleToShow);
-    }
+        setFilterString(event.target.value);
+    };
+
+    const peopleToShow = (filterString.length > 0)
+        ? people.filter(person =>
+            person.name.toLowerCase().startsWith(filterString.toLowerCase()))
+        : people;
 
     return (
         <div>
             <h2>Phonebook</h2>
 
-            <div>filter shown with <input onChange={filterPeople}/></div>
+            <div>filter shown with <input value={filterString} onChange={filterPeople} /></div>
 
             <h3>Add a new person</h3>
             <form onSubmit={addPerson}>
-                <div>name: <input value={newName} onChange={handleNameChange}/></div>
-                <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+                <div>name: <input value={newName} onChange={handleNameChange} /></div>
+                <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
                 <div><button type="submit">add</button></div>
             </form>
             <h2>Numbers</h2>
-            <div>debug: {newName}</div>
-            <People people={people}/>
+            <ul>
+                {peopleToShow.map(person =>
+                    <Person key={person.name} person={person} />
+                )}
+            </ul>
         </div>
     );
 
