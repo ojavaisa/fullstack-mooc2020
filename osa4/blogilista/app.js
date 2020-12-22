83f5977df9c28ad85e4f00751dc2fc23f49a4dbb
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-22 16:05:31 +0200

    Tehtävä 4.2. step 3
    Konfiguraatio omaan moduuliin

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -14,7 +14,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
     logger.info('connected to MongoDB');
