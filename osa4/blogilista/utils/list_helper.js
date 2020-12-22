b46846206e2df17791646657c44d5bd56a2f0455
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-22 18:30:49 +0200

    Tehtävät 4.4. & 4.5.
    Funktiot totalLikes ja favoriteBlog sekä niiden testaus

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
-  return 1;
+const totalLikes = (blogs) => {
+
+  return blogs.map(blog => blog.likes).reduce((sum, item) => sum + item, 0);
+};
+
+const favoriteBlog = (blogs) => {
+
+  return blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current, {});
 };
 
 module.exports = {
-  dummy
+  totalLikes,
+  favoriteBlog
 };
