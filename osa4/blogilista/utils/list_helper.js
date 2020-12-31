d7f2f5bd7e49fc6efafe12d13cf56ea90d95f494
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-31 18:55:31 +0200

    Tehtävä 4.6. alku
    mostBlogs-funktio tehty, testit puuttuvat

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');  //more verstile collection handling functions
+
 const totalLikes = (blogs) => {
 
   return blogs.map(blog => blog.likes).reduce((sum, item) => sum + item, 0);
@@ -8,7 +10,15 @@ const favoriteBlog = (blogs) => { //return blog with most likes, returns empty o
   return blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current, {});
 };
 
+const mostBlogs = (blogs) => {
+  const blogCounts = _.countBy(blogs, 'author');  //lodash: count objects by 'author' attribute, returns object that has author names as keys and counts as values
+  const blogCountsObjs = _.map(blogCounts,
+    (val, key) => ({ author: key, blogs: val }) );  //turn previous object into objects with author names and blogcounts
+  return _.maxBy(blogCountsObjs, 'blogs');  //get maximum of previous objects by blog count
+};
+
 module.exports = {
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 };
