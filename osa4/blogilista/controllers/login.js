b05b2d7b0b5343cf21f75ab83ffc74d720ff57f3
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-31 15:59:46 +0200

    Tehtävät 4.19. & 4.20.
    Blogin lisäys onnistuu vain validin tokenin kanssa. Tokenin ekstraktointi toteutettu suoraan middlewarena

--- osa4/blogilista/controllers/login.js
+++ osa4/blogilista/controllers/login.js
@@ -19,7 +19,7 @@ loginRouter.post('/', async (request, response) => {
 
   const userForToken = {
     username: user.username,
-    id: user._id,
+    id: user._id, //NOTE: using db assigned id (object)
   };
 
   const token = jwt.sign(userForToken, process.env.SECRET);
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -19,13 +19,28 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' });
   }
 
   next(error);
 };
 
+const tokenExtractor = (req, res, next) => {
+  //extract token from Authorization header and add to request
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    req.token = authorization.substring(7);  //return authorization string without 'bearer '
+  } else {
+    req.token = null;
+  }
+
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 };
