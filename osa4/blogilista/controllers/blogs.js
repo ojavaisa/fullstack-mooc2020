d00ce504c0cd0277eb3d5c9bd71819f7bcbc4789
Olli Väisänen <olli.vaisanen@iki.fi>
2021-01-04 19:37:09 +0200

    Tehtävä 4.14.
    Blogien päivitys toteutettu likejen osalta. Toimivuus testattu VS Code REST Clientilla

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -67,4 +67,15 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 });
 
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  response.json(updatedBlog.toJSON());
+});
+
 module.exports = blogsRouter;
