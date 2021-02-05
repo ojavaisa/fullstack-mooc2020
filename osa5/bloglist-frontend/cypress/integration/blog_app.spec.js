4583c93259ee7cd381f1fd3553930babfeb03f41
Olli Väisänen <olli.vaisanen@iki.fi>
2021-02-05 18:43:39 +0200

    Tehtävä 5.18.
    Virhenotifikaation testausta ei vielä toteutettu (bonustehtävä), koska itse notifikaatioita ei ole vielä toteutettu (tehtävä 5.4.*)

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    const user = {
+      name: 'Mauri Ernesti',
+      username: 'mauriernesti',
+      password: 'supersecret'
+    };
+    cy.request('POST', 'http://localhost:3001/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -8,8 +14,31 @@ describe('Blog app', function () {
 
     cy.get('h2')
       .should('contain', 'Blogs');
-    
+
     cy.get('#username');  // cypress commands have built-in assertions, so no need for ".should('exist')" or something similar
-    cy.get('#password')
+    cy.get('#password');
   });
-})
+
+  describe('Login', function () {
+    it('succeeds with correct credentielas', function () {
+      cy.get('#username').type('mauriernesti');
+      cy.get('#password').type('supersecret');
+      cy.get('#login-button').click();
+
+      cy.contains('Mauri Ernesti logged in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mauriernesti');
+      cy.get('#password').type('wrongpassword');
+      cy.get('#login-button').click();
+
+      /* cy.get('.error') //error notificatins not yet implemented in frontend
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid'); */
+      
+      cy.get('html').should('not.contain', 'Mauri Ernesti logged in')
+    });
+  });
+});
