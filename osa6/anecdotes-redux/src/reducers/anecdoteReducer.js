9b97ca7ec67290ca3fe2a021eccda6a3c67448e5
Olli Väisänen <olli.vaisanen@iki.fi>
2021-02-15 18:16:24 +0200

    Tehtävä 6.4.

--- osa6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ osa6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -7,7 +7,7 @@ const anecdotesAtStart = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
 
-const getId = () => (100000 * Math.random()).toFixed(0);
+const getId = () => (1000000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
@@ -26,6 +26,17 @@ export const voteAnecdote = (id) => {
   };
 };
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content: content,
+      id: getId(),
+      votes: 0
+    }
+  };
+};
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state);
   console.log('action', action);
@@ -38,9 +49,11 @@ const reducer = (state = initialState, action) => {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1
       };
-      return state.map(anecdote => 
+      return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote
       );
+    case 'NEW_ANECDOTE':
+      return state.concat(action.data);
     default:
       return state;
   }
