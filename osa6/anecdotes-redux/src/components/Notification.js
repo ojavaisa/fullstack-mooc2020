44846cecb92ffe56c6d5c28269aa017f35f329f5
Olli Väisänen <olli.vaisanen@iki.fi>
2021-03-14 17:35:06 +0200

    Tehtävä 6.19.

--- osa6/anecdotes-redux/src/components/Notification.js
+++ osa6/anecdotes-redux/src/components/Notification.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+//import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification);
+const Notification = (props) => {
+  //const notification = useSelector(state => state.notification);
   const style = {
     border: 'solid',
     padding: 10,
@@ -10,9 +11,17 @@ const Notification = () => {
   };
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   );
 };
 
-export default Notification;
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+//export default Notification;
+const ConnectedNotification = connect(mapStateToProps)(Notification);
+export default ConnectedNotification;
